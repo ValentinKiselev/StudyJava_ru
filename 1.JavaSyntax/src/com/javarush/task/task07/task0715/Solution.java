b72d533783b42075d34ad75837c5c8e8264aba22
Valentin <valentin_kiselev@iszf.irk.ru>
2019-01-19 00:50:05 +0800

    ArrayList task15

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0715/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task07.task0715;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Продолжаем мыть раму
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("мама");
+        strings.add("мыла");
+        strings.add("раму");
+        strings.add(1,"именно");
+        strings.add(3,"именно");
+        strings.add(5,"именно");
+        for(int i = 0; i < strings.size(); i++){
+            System.out.println(strings.get(i));
+        }
+    }
+}
