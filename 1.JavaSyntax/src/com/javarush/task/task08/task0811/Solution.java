18f7921e415c8f4351b389bdd995f9f07795fbad
Valentin <valentin_kiselev@iszf.irk.ru>
2019-01-27 22:16:24 +0800

    getInsertTimeInMs2

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0811/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task08.task0811;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Квартет «Методы»
+*/
+
+public class Solution {
+    public static List getListForGet() {
+        ArrayList al1 = new ArrayList();
+        return al1;
+
+    }
+
+    public static List getListForSet() {
+        ArrayList al1 = new ArrayList();
+        return al1;
+
+    }
+
+    public static List getListForAddOrInsert() {
+        LinkedList ll1 = new LinkedList();
+        return ll1;
+    }
+
+    public static List getListForRemove() {
+        LinkedList ll1 = new LinkedList();
+        return ll1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
