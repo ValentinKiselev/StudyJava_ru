a3016e5ddb37e3ae5531ec7a12fe9f3239ef3aeb
Valentin <valentin_kiselev@iszf.irk.ru>
2019-04-09 01:04:40 +0800

    Instanceof7

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1404/Solution.java
@@ -0,0 +1,79 @@
+package com.javarush.task.task14.task1404;
+
+/* 
+Коты
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Cat> catlist = new ArrayList<>();
+
+        while (true){
+            String bu = buff.readLine();
+            catlist.add(CatFactory.getCatByKey(bu));
+            if (bu.equals("")) break;
+        }
+
+
+        for(int j = 0; j < catlist.size()-1; j++){
+        System.out.println(catlist.get(j).toString());
+        }
+    }
+
+    static class CatFactory {
+        static Cat getCatByKey(String key) {
+            Cat cat = null;
+            if ("vaska".equals(key)) {
+                cat = new MaleCat("Василий");
+            } else if ("murka".equals(key)) {
+                cat = new FemaleCat("Мурочка");
+            } else if ("kiska".equals(key)) {
+                cat = new FemaleCat("Кисюлька");
+            } else {
+                cat = new Cat(key);
+            }
+            return cat;
+        }
+    }
+
+    static class Cat {
+        private String name;
+
+        protected Cat(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String toString() {
+            return "Я уличный кот " + getName();
+        }
+    }
+
+    static class MaleCat extends Cat {
+        MaleCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "Я - солидный кошак по имени " + getName();
+        }
+    }
+
+    static class FemaleCat extends Cat {
+        FemaleCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "Я - милая кошечка по имени " + getName();
+        }
+    }
+}
