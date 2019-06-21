807e93ad3eee269ffce283c6b39d7c9b796e9c4b
Valentin <valentin_kiselev@iszf.irk.ru>
2019-06-21 09:00:05 +0800

    LongWordsv2

--- 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+
         BufferedReader reader = new BufferedReader(new FileReader(args[0]));
         BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
 
