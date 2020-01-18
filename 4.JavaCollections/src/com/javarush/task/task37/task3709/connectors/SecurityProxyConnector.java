75f88e505f73550e09a369cf3db121f7427f7a65
valki <valentin_kiselev@iszf.irk.ru>
2020-01-18 14:34:31 +0800

    SecurityProxyConnector

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3709/connectors/SecurityProxyConnector.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task37.task3709.connectors;
+
+import com.javarush.task.task37.task3709.security.SecurityChecker;
+
+public class SecurityProxyConnector implements Connector {
+    SecurityChecker securityChecker;
+    SimpleConnector simpleConnector;
+
+    public SecurityProxyConnector(String string) {
+        this.simpleConnector = new SimpleConnector(string);
+    }
+    @Override
+    public void connect() {
+        if(securityChecker.performSecurityCheck()) simpleConnector.connect();
+    }
+}
