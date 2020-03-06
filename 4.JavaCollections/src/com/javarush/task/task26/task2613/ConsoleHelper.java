f0ce78f11ff3e01cb4e942f6abb270661b9fdd6f
valki <valentin_kiselev@iszf.irk.ru>
2020-03-06 20:13:49 +0800

    CashMachine2

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -1,4 +1,24 @@
 package com.javarush.task.task26.task2613;
 
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+    public static String readString() throws InterruptOperationException {
+        String message = "";
+        try {
+            message = bis.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
 }
