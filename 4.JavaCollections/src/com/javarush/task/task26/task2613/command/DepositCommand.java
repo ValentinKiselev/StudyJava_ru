fba013ed04cd9a3765ea274d92a3f0f2cb4ccc9e
valki <valentin_kiselev@iszf.irk.ru>
2020-03-17 21:32:13 +0800

    CashMachine13

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/DepositCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/DepositCommand.java
@@ -1,15 +1,28 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.CashMachine;
 import com.javarush.task.task26.task2613.ConsoleHelper;
 import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
 import com.javarush.task.task26.task2613.exception.InterruptOperationException;
 
+import java.util.ResourceBundle;
+
 class DepositCommand implements Command{
+
+    private ResourceBundle res = ResourceBundle.getBundle(CashMachine.class.getPackage().getName() + ".resources.deposit_en");
 
     @Override
     public void execute() throws InterruptOperationException {
-        String code = ConsoleHelper.askCurrencyCode();
-        String[] value = ConsoleHelper.getValidTwoDigits(code);
-        CurrencyManipulatorFactory.getManipulatorByCurrencyCode(code).addAmount(Integer.parseInt(value[0]), Integer.parseInt(value[1]));
+        ConsoleHelper.writeMessage(res.getString("before"));
+        try {
+            String code = ConsoleHelper.askCurrencyCode();
+
+            String[] value = ConsoleHelper.getValidTwoDigits(code);
+            CurrencyManipulatorFactory.getManipulatorByCurrencyCode(code).addAmount(Integer.parseInt(value[0]), Integer.parseInt(value[1]));
+            ConsoleHelper.writeMessage(String.format(res.getString("success.format"), Integer.parseInt(value[0]) * Integer.parseInt(value[1]), code));
+        }
+        catch (NumberFormatException e) {
+            ConsoleHelper.writeMessage(res.getString("invalid.data"));
+        }
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/InfoCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/InfoCommand.java
@@ -1,14 +1,18 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.CashMachine;
 import com.javarush.task.task26.task2613.ConsoleHelper;
 import com.javarush.task.task26.task2613.CurrencyManipulator;
 import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
 
+import java.util.ResourceBundle;
+
 class InfoCommand implements Command  {
+    private ResourceBundle res = ResourceBundle.getBundle(CashMachine.class.getPackage().getName() + ".resources.info_en");
     @Override
     public void execute() {
         boolean moneystatus = false;
-
+        ConsoleHelper.writeMessage(res.getString("before"));
         for (CurrencyManipulator current : CurrencyManipulatorFactory.getAllCurrencyManipulators()) {
             if (current.hasMoney()) {
                 if (current.getTotalAmount() > 0) {
@@ -18,7 +22,7 @@ class InfoCommand implements Command  {
             }
         }
         if (!moneystatus) {
-            ConsoleHelper.writeMessage("No money available.");
+            ConsoleHelper.writeMessage(res.getString("no.money"));
         }
     }
 
