b184da0311b950883800f859950a7d52e1639827
valki <valentin_kiselev@iszf.irk.ru>
2020-04-19 14:25:08 +0800

    Hippodrome6

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,9 +1,12 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
-    private List<Horse> horses;
+    private List<Horse> horses = new ArrayList<>();
+    static Hippodrome game;
+
 
     public List<Horse> getHorses() {
         return horses;
@@ -13,6 +16,17 @@ public class Hippodrome {
         this.horses = horses;
     }
 
+    public Hippodrome() {
+        this.horses = horses;
+    }
+
     public static void main(String[] args) {
+        game = new Hippodrome();
+        Horse horse1 = new Horse("Plyashka", 3, 0);
+        Horse horse2 = new Horse("Hryashka", 3, 0);
+        Horse horse3 = new Horse("Nyashka", 3, 0);
+        game.getHorses().add(horse1);
+        game.getHorses().add(horse2);
+        game.getHorses().add(horse3);
         }
 }
