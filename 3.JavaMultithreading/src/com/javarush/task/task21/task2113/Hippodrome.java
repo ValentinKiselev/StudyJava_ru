9b68c8dc8fbdf087d05ff829d7589177d588f218
valki <valentin_kiselev@iszf.irk.ru>
2020-04-19 14:31:15 +0800

    Hippodrome7

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -20,8 +20,12 @@ public class Hippodrome {
         this.horses = horses;
     }
 
-    public void run(){
-
+    public void run() throws InterruptedException {
+    for(int i = 0; i < 100; i++) {
+        move();
+        print();
+        Thread.sleep(200);
+    }
     }
 
     public void print(){
