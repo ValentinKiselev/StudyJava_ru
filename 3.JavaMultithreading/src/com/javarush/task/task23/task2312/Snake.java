8886b9d4bd251a6bcf9e5c12b26a3eec2c9a320c
valki <valentin_kiselev@iszf.irk.ru>
2020-05-05 13:12:30 +0800

    Snake8

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,11 +1,14 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
     private List<SnakeSection> sections;
     private boolean isAlive;
     private SnakeDirection direction;
+    private int x;
+    private int y;
 
     public void setDirection(SnakeDirection direction) {
         this.direction = direction;
@@ -21,5 +24,23 @@ public class Snake {
 
     public SnakeDirection getDirection() {
         return direction;
+    }
+
+    public int getX() { return sections.get(0).getX(); }
+
+    public int getY() {
+        return sections.get(0).getY();
+    }
+
+    public void move() {
+
+    }
+
+    public Snake(int x, int y) {
+        this.sections = sections;
+        this.isAlive = true;
+        sections = new ArrayList<>();
+        SnakeSection snakehead = new SnakeSection(x, y);
+        sections.add(snakehead);
     }
 }
