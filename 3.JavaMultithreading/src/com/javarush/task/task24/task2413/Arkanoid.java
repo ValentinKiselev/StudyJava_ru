affed9b68db84b7ad843e92e5b2cc4993f79de4f
valki <valentin_kiselev@iszf.irk.ru>
2020-05-20 13:21:10 +0800

    Arkanoid4

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,6 +1,36 @@
 package com.javarush.task.task24.task2413;
 
+import java.util.List;
+
 public class Arkanoid {
+   private Ball ball;
+   private Stand stand;
+   private List<Brick> bricks;
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
+    }
+
     public Arkanoid(int width, int height) {
         this.width = width;
         this.height = height;
