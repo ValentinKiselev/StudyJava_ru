9ea00bb28b3db705ad9792528225832bed2c0224
valki <valentin_kiselev@iszf.irk.ru>
2020-05-25 13:01:44 +0800

    Arkanoid13

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -1,6 +1,34 @@
 package com.javarush.task.task24.task2413;
 
 public class Ball extends BaseObject {
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen = true;
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
     public Ball(double x, double y, double radius) {
         super(x, y, radius);
     }
