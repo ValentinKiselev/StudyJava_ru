976e29cda209873e95b800954fb4fbde07665d9d
valki <valentin_kiselev@iszf.irk.ru>
2020-05-26 12:22:27 +0800

    Arkanoid16

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,8 +1,31 @@
 package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject{
+    private double speed;
+    private double direction;
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
+    }
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
+
     public Stand(double x, double y, double radius) {
         super(x, y, radius);
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
     }
 
     @Override
