90075be54c20717a2c40a4287b26c4f5ed634ebe
valki <valentin_kiselev@iszf.irk.ru>
2020-06-09 11:41:18 +0800

    MVCExample3

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task36.task3608.view;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
+import java.util.List;
+
+public class UsersView implements View {
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("All users:");
+        List<User> users = modelData.getUsers();
+
+        for (User user: users)
+            System.out.println("\t" + user);
+            System.out.println("===================================================");
+    }
+
+    public void fireEventShowAllUsers(){
+        controller.onShowAllUsers();
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+}
