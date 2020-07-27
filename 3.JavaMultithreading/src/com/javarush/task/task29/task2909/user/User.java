fe579bd88f1d5538b0ee02a7e41431de5e980afa
valki <valentin_kiselev@iszf.irk.ru>
2020-07-27 21:23:56 +0800

    RefactoringExample14

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -4,10 +4,9 @@ public class User {
     private String name;
     private String surname;
     private int age;
+    private boolean man;
 
-    private String country;
-    private String city;
-    private House house;
+    private Address address;
 
     private Work work;
 
@@ -15,17 +14,6 @@ public class User {
         this.name = name;
         this.surname = surname;
         this.age = age;
-    }
-
-    public void printInfo(){
-        System.out.println("Имя: " + name);
-        System.out.println("Фамилия: " + surname);
-    }
-    public void printAdditionalInfo() {
-        if (age < 16)
-            System.out.println("Пользователь моложе 16 лет");
-        else
-            System.out.println("Пользователь старше 16 лет");
     }
 
     public String getName() {
@@ -53,23 +41,23 @@ public class User {
     }
 
     public String getCountry() {
-        return country;
+        return address.getCountry();
     }
 
     public void setCountry(String country) {
-        this.country = country;
+        address.setCountry(country);
     }
 
     public String getCity() {
-        return city;
+        return address.getCity();
     }
 
     public void setCity(String city) {
-        this.city = city;
+        address.setCity(city);
     }
 
     public String getAddress() {
-        return country + " " + city + " " + house.house;
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
     }
 
     public Work getWork() {
@@ -78,5 +66,30 @@ public class User {
 
     public void setWork(Work work) {
         this.work = work;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
+    }
+
+    public void printInfo() {
+        System.out.println("Имя: " + getName());
+        System.out.println("Фамилия: " + getSurname());
+        printAdditionalInfo();
+    }
+
+    public void printAdditionalInfo() {
+        if (getAge() < 16)
+            System.out.println("Пользователь моложе 16 лет");
+        else
+            System.out.println("Пользователь старше 16 лет");
+    }
+
+    public String getBoss() {
+        return getWork().getBoss();
     }
 }
