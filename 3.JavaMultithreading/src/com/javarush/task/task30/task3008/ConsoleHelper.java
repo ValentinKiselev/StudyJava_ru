ed8beb241bb877f8ddb7e32d65068ad98975c628
valki <valentin_kiselev@iszf.irk.ru>
2020-08-05 13:06:30 +0800

    Chat2

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,40 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
-}
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+    public static String readString(){
+        String string = null;
+        boolean readed = false;
+        while (!readed) {
+            try {
+                string = bufferedReader.readLine();
+                readed = true;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return string;
+    }
+    public static int readInt(){
+        int num = 0;
+        boolean readed = false;
+        while (!readed) {
+            try {
+                num = Integer.parseInt(readString());
+                readed = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return num;
+    }
+    }
+
