e210f87bfff67cd7e465978a02c405a4b4160659
valki <valentin_kiselev@iszf.irk.ru>
2020-08-07 12:28:48 +0800

    Chat6

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,32 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) throws Exception{
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            super();
+            this.socket = socket;
+        }
     }
+
+    public static void main(String[] args) throws Exception{
+        ConsoleHelper.writeMessage("Номер порта:");
+        int portNum = ConsoleHelper.readInt();
+        try(ServerSocket serverSocket = new ServerSocket(portNum)) {
+            ConsoleHelper.writeMessage("Сервер запущен на порту: " + portNum);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        }
+        catch (Exception e) {
+            ConsoleHelper.writeMessage(e.getMessage());
+        }
+    }
+
 }
