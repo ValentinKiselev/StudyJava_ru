66e61ddebad4d3a948714dd98f664c62fa566ad8
valki <valentin_kiselev@iszf.irk.ru>
2020-08-12 10:34:13 +0800

    Chat7

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,9 +1,14 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+
+    static private Map<String, Connection> connectionMap = new ConcurrentHashMap<>();;
 
     private static class Handler extends Thread{
         private Socket socket;
@@ -26,6 +31,16 @@ public class Server {
         }
         catch (Exception e) {
             ConsoleHelper.writeMessage(e.getMessage());
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message){
+        for (String clientName : connectionMap.keySet()) {
+            try {
+                connectionMap.get(clientName).send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Не могу отправить сообщение клиенту с именем: " + clientName);
+            }
         }
     }
 
