4b4461bc262814e0e1b07a88748e9e3abdae5a72
valki <valentin_kiselev@iszf.irk.ru>
2020-08-15 13:28:09 +0800

    Chat14

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -10,6 +10,38 @@ import java.io.IOException;
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    public void run() {
+        {
+            SocketThread socketThread = getSocketThread();
+            socketThread.setDaemon(true);
+            socketThread.start();
+            try {
+                synchronized (this) {
+                    wait();
+                }
+            } catch (InterruptedException e) {
+                ConsoleHelper.writeMessage("Ошибка ожидания потока");
+                System.exit(1);
+            }
+            if (clientConnected) {
+                ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+                while (clientConnected) {
+                    String message = ConsoleHelper.readString();
+                    if (message.equalsIgnoreCase("exit")) {
+                        break;
+                    } else {
+                        if (shouldSendTextFromConsole()) {
+                            sendTextMessage(message);
+                        }
+                    }
+                }
+            } else {
+                ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+            }
+        }
+    }
+
     public static class SocketThread extends Thread{
 
     }
@@ -29,9 +61,12 @@ public class Client {
     protected boolean shouldSendTextFromConsole(){
         return true;
     }
-    protected SocketThread getSocketThread(){
+
+    protected SocketThread getSocketThread()
+    {
         return new SocketThread();
     }
+
     protected void sendTextMessage(String text){
         try {
             connection.send(new Message(MessageType.TEXT, text));
@@ -40,4 +75,10 @@ public class Client {
             clientConnected = false;
         }
     }
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+
+
 }
