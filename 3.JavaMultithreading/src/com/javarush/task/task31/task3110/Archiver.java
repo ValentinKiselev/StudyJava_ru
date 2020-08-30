5391711d6d274b70b608b0f09da0c57dd1f4173c
valki <valentin_kiselev@iszf.irk.ru>
2020-08-30 21:05:17 +0800

    Archiver1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.nio.file.Paths;
+
+public class Archiver {
+
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите, пожалуйста, путь.");
+        String text = null;
+        try {
+            text = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(text));
+        try {
+            String name = bufferedReader.readLine();
+            zipFileManager.createZip(Paths.get(new URI(name)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
