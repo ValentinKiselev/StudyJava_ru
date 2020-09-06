7624e3320421bf8dcf8f7ac6011acc3c221071c5
valki <valentin_kiselev@iszf.irk.ru>
2020-09-06 13:41:11 +0800

    Archiver13

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,13 +1,17 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
@@ -69,4 +73,21 @@ public class ZipFileManager {
             out.write(buffer, 0, len);
         }
     }
+
+    public List<FileProperties> getFilesList() throws Exception{
+        if(!Files.isRegularFile(zipFile)) throw new WrongZipFileException();
+        List<FileProperties> files = new ArrayList<>();
+        try(ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))){
+            ZipEntry zipEntry = zipInputStream.getNextEntry();
+            while (zipEntry!=null){
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                copyData(zipInputStream, byteArrayOutputStream);
+                FileProperties file = new FileProperties(zipEntry.getName(), zipEntry.getSize(), zipEntry.getCompressedSize(), zipEntry.getMethod());
+                files.add(file);
+                zipEntry = zipInputStream.getNextEntry();
+            }
+        }
+        return files;
+    }
+
 }
