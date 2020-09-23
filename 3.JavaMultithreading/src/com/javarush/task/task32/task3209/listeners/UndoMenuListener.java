97cb778fe1cc1d5b91c64c803f08a5f49c77b152
valki <valentin_kiselev@iszf.irk.ru>
2020-09-23 19:43:37 +0800

    HTMLEditor10

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/UndoMenuListener.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/UndoMenuListener.java
@@ -7,12 +7,25 @@ import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 
 public class UndoMenuListener implements MenuListener {
-    View view;
-    JMenuItem undoMenuItem;
-    JMenuItem redoMenuItem;
+    private View view;
+    private JMenuItem undoMenuItem;
+    private JMenuItem redoMenuItem;
+
     @Override
     public void menuSelected(MenuEvent e) {
+        if (view.canUndo()) {
+            undoMenuItem.setEnabled(true);
+        }
+        else if (!view.canUndo()) {
+            undoMenuItem.setEnabled(false);
+        }
 
+        if (view.canRedo()) {
+            redoMenuItem.setEnabled(true);
+        }
+        else if (!view.canRedo()) {
+            redoMenuItem.setEnabled(false);
+        }
     }
 
     @Override
