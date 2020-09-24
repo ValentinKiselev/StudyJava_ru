914670ada4d2a916366e9061754c719f1d58303d
valki <valentin_kiselev@iszf.irk.ru>
2020-09-24 23:28:11 +0800

    HTMLEditor12

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/RedoAction.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/RedoAction.java
@@ -6,13 +6,13 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class RedoAction extends AbstractAction {
-    View view;
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
+    private View view;
 
     public RedoAction(View view) {
         this.view = view;
+    }
+    @Override
+    public void actionPerformed(ActionEvent actionEvent){
+        view.redo();
     }
 }
