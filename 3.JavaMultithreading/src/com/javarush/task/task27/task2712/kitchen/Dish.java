487842985a48744274200b946426e7e26ccda54f
valki <valentin_kiselev@iszf.irk.ru>
2020-10-08 20:38:26 +0800

    Restaurant1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Dish.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task27.task2712.kitchen;
+
+public enum Dish {
+    Fish,
+    Steak,
+    Soup,
+    Juice,
+    Water;
+    public static String allDishesToString(){
+        return "Fish, Steak, Soup, Juice, Water";
+    }
+
+}
