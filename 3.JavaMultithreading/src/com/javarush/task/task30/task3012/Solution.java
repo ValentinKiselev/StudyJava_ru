ee076318a702dbdaa9f453d5ad209b0fff3dd677
valki <valentin_kiselev@iszf.irk.ru>
2020-10-24 14:56:58 +0800

    ToricSystemTask

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3012/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task30.task3012;
+
+/* 
+Получи заданное число
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.createExpression(74);
+    }
+
+    public void createExpression(int number) {
+        int[] arr = {1, 3, 9, 27, 81, 243, 729, 2187};
+        String s = "";
+        int num = number;
+        while (num > 0)
+        {
+            if (num % 3 == 0)
+            {
+                s = s + "0";
+                num/=3;
+            }
+            else if (num % 3 == 1)
+            {
+                s = s + "+";
+                num/=3;
+            }
+            else if (num % 3 == 2)
+            {
+                s = s + "-";
+                num/=3;
+                num++;
+            }
+        }
+        int sum = 0;
+        String str = "";
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s.charAt(i) == '+') {
+                str += " + " + arr[i];
+                sum+=arr[i];
+            }
+            else if (s.charAt(i) == '-')
+            {
+                str += " - " + arr[i];
+                sum -= arr[i];
+            }
+        }
+        System.out.println(sum + " =" + str);
+    }
+}
